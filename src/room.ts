8f49872a5c167744c4f57e062fce75007089cdf0
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-12-27 15:57:10 +0000

    feat: enhance Room interface with uniqueIdentifier and accessories properties

--- src/room.ts
+++ src/room.ts
@@ -1,4 +1,20 @@
+import { Accessory } from './accessory';
+
 /**
  * The smallest subdivision of a home’s space.
  */
-export interface Room {}
+export interface Room {
+  // Identifying a room
+
+  /**
+   * The unique identifier for a room.
+   */
+  uniqueIdentifier: string;
+
+  // Finding accessories
+
+  /**
+   * The collection of accessories in the room.
+   */
+  accessories: Accessory[];
+}
