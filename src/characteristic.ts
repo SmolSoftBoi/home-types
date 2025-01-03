0a736dc80b5d1250426674655c2092d7d4d6cd7f
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-01-03 21:12:33 +0000

    feat: add uniqueIdentifier and localizedDescription properties to Characteristic interface

--- src/characteristic.ts
+++ src/characteristic.ts
@@ -2,6 +2,18 @@
  * A specific characteristic of a service, like the brightness of a dimmable light or its color temperature.
  */
 export interface Characteristic {
+  // Identifying a characteristic
+
+  /**
+   * A unique identifier for the characteristic.
+   */
+  uniqueIdentifier: string;
+
+  /**
+   * The localized description of the characteristic.
+   */
+  localizedDescription: string;
+
   // Specifying units
 
   /**
